d6976b10fdb5884582801af3efcdf4515c2b9885
Oleh Volchkov <ovolc@softserveinc.com>
2019-08-13 18:01:48 +0300

    fixed logic

--- dpppull.sh
+++ dpppull.sh
@@ -6,12 +6,24 @@ DIRS=("C:/DPPSource/dpp-platform-base" "C:/DPPSource/dpp8" "C:/DPPSource/dpp7")
 pull() {
     REPODIR=$1
     cd ${REPODIR}
+    # BRANCH=$(git br | grep \* | cut -d ' ' -f2)
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
     REPO=$(git remote show -n origin | grep Push | cut -d: -f2- | cut -d/ -f5-) &&
         printf "\n>${REPO}\n"
-    git pull origin master &&
-        if $MERGE; then
-            git merge master
-        fi
+    if [ "$BRANCH" != master ]; then
+        git stash &&
+            git co master -q &&
+            printf "Switched to master.\n"
+        git pull &&
+            git co ${BRANCH} -q &&
+            printf "Switched back to $BRANCH.\n"
+        git stash pop &&
+            if $MERGE; then
+                git merge master
+            fi
+    else
+        git pull
+    fi
 }
 
 for arg in "$@"; do
